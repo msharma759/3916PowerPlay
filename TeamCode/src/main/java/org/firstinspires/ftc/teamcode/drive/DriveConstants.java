4aab07d33c7c90ec703dceb06fabd9ba225f973b
team3916 <58050738+team3916@users.noreply.github.com>
2023-01-12 15:52:04 -0500

    FeedForward Tuning

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/DriveConstants.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/DriveConstants.java
@@ -55,8 +55,8 @@ public class DriveConstants {
      * motor encoders or have elected not to use them for velocity control, these values should be
      * empirically tuned.
      */
-    public static double kV = 1.0 / rpmToVelocity(MAX_RPM);
-    public static double kA = 0;
+    public static double kV = 0.016569832296350887;
+    public static double kA = 0.0025;
     public static double kStatic = 0;
 
     /*
