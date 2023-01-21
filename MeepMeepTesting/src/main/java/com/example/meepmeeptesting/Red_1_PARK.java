d214515bad71b9ac90e6f4a8679295e394d3c5b2
Proprixia <77602359+Proprixia@users.noreply.github.com>
2023-01-21 10:00:35 -0500

    Added parking paths in MeepMeep and opmode

---
+++ MeepMeepTesting/src/main/java/com/example/meepmeeptesting/Red_1_PARK.java
@@ -0,0 +1,31 @@
+package com.example.meepmeeptesting;
+
+import com.acmerobotics.roadrunner.geometry.Pose2d;
+import com.noahbres.meepmeep.MeepMeep;
+import com.noahbres.meepmeep.roadrunner.DefaultBotBuilder;
+import com.noahbres.meepmeep.roadrunner.entity.RoadRunnerBotEntity;
+
+/**
+ * @author Jason Armbruster
+ */
+
+public class Red_1_PARK {
+    public static void main(String[] args) {
+        MeepMeep meepMeep = new MeepMeep(800);
+
+        RoadRunnerBotEntity myBot = new DefaultBotBuilder(meepMeep)
+                // Set bot constraints: maxVel, maxAccel, maxAngVel, maxAngAccel, track width
+                .setConstraints(60, 60, Math.toRadians(180), Math.toRadians(180), 18)
+                .followTrajectorySequence(drive ->
+                                drive.trajectorySequenceBuilder(new Pose2d(-35.00, -60.00, Math.toRadians(90)))
+                                        .strafeLeft(24.0)
+                                        .build()
+                );
+
+        meepMeep.setBackground(MeepMeep.Background.FIELD_POWERPLAY_OFFICIAL)
+                .setDarkMode(true)
+                .setBackgroundAlpha(0.95f)
+                .addEntity(myBot)
+                .start();
+}
+}
