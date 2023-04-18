1deaa20f900b6af7578d52f070549babe4863d09
team3916 <58050738+team3916@users.noreply.github.com>
2023-04-18 15:57:12 -0400

    Started work on TOML parser

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/tomlpaths/TOMLToPathParser.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/tomlpaths/TOMLToPathParser.java
@@ -2,6 +2,7 @@ package org.firstinspires.ftc.teamcode.tomlpaths;
 
 import android.annotation.SuppressLint;
 
+import org.firstinspires.ftc.teamcode.trajectorysequence.TrajectorySequence;
 import org.tomlj.Toml;
 import org.tomlj.TomlTable;
 
@@ -9,15 +10,20 @@ import java.io.File;
 import java.util.Vector;
 
 public class TOMLToPathParser {
-    Vector<TomlTable> tomlPaths = new Vector();
+    Vector<TomlTable> tomlSequences = new Vector();
 
     @SuppressLint("NewApi")
+//    public TOMLToPathParser() {
+//        File dir = new File(".");
+//        for (File file : dir.listFiles()) {
+//            if(file.getName().endsWith(".toml")) {
+//                tomlSequences.add(Toml.parse(file.getPath()).getTable("sequence");
+//
+//            }
+//        }
+//    }
     public TOMLToPathParser() {
-        File dir = new File(".");
-        for (File file : dir.listFiles()) {
-            if(file.getName().endsWith(".toml")) {
-                tomlPaths.add(Toml.parse(file.getPath()).getTable("moves"));
-            }
-        }
+        File file = new File("~/TeamCode/java/tomlpaths/Blue_1_1.toml");
+        tomlSequences.add(Toml.parse(file.getPath()).getTable("sequence"));
     }
 }
